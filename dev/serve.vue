83f9c6080f8b8a66f67512fe1892c7e795bd82f0
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-07-18 11:53:08 +0100

    Run build

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,18 @@
 <script>
 import Vue from 'vue';
-import SkeletonLoaderVueSample from '@/entry';
+// import SkeletonLoaderVueSample from '@/entry';
 
 export default Vue.extend({
   name: 'ServeDev',
-  components: {
-    SkeletonLoaderVueSample,
-  }
+  // components: {
+  //   SkeletonLoaderVueSample,
+  // }
 });
 </script>
 
 <template>
   <div id="app">
-    <skeleton-loader-vue-sample type="circle" />
-    <skeleton-loader-vue-sample type="rect" height="120" width="250" rounded radius="8" color="red" />
+    <!-- <skeleton-loader-vue-sample type="circle" /> -->
+    <!-- <skeleton-loader-vue-sample type="rect" height="120" width="250" rounded radius="8" color="red" /> -->
   </div>
 </template>
